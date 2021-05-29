58b4bed3f116c5b231b69f97f120543f74f39df8
Your Name <you@example.com>
2021-05-28 21:23:36 -0400

    Ajustes em aula

---
+++ src/main/java/br/senai/sc/denicio/classes/ArvoreComArray.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.senai.sc.denicio.classes;
+
+/**
+ *
+ * @author Aluno
+ * @param <T>
+ */
+public class ArvoreComArray<T> {
+    int h;
+    T[] array;
+    
+    public ArvoreComArray(int h) {
+        this.h = h;
+        
+    }
+    
+}
